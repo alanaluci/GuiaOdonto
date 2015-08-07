7a78b6a17c2657cd086345f792d8166f6648bdd7
alanaluci <alanaluciaso@gmail.com>
2015-08-07 19:41:37 -0300

    Adicionei o que jaime passou por pen drive tentativa de colocar na listview -adicionou servicehandler -adicionou Estados -modificou a MainActivty

--- app/src/main/java/maddo/foo/telasplash/MainActivity.java
+++ app/src/main/java/maddo/foo/telasplash/MainActivity.java
@@ -7,7 +7,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 
 public class MainActivity extends ActionBarActivity {
@@ -68,6 +92,119 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+/*
+**
+**
+* DAQUI PRA CIMA NÃO MEXER - DAQUI PRA BAIXO É TENTATIVA DE COLOCAR O BANCO DE DADOS NA LISTVIEW
+**
+**
+ */
+    private TextView txtCategory;
+    private Spinner spinnerFood;
+    // array list for spinner adapter
+    private ArrayList<Estados> estadosList;
+    ProgressDialog pDialog;
+    // API urls
+    // Url to create new category
+    //private String URL_NEW_CATEGORY = "http://10.0.2.2/food_api/new_category.php";
+    // Url to get all categories
+    private String URL_ESTADOS = "http://servdonto.no-ip.org/prestadores/android/guia/index1.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        listview = (ListView) findViewById(R.id.ListView);
+        txtEstados = (TextView) findViewById(R.id.txtCategory);
+
+        estadosList = new ArrayList<Estados>();
+
+        // spinner item select listener
+        listview.setOnItemSelectedListener(this);
+
+        new GetEstados().execute();
+
+    }
+
+    /**
+     * Adding spinner data
+     * */
+    private void populateTextView() {
+        List<String> lables = new ArrayList<String>();
+
+        txtEstados.setText("");
+
+        for (int i = 0; i < estadosList.size(); i++) {
+            lables.add(estadosList.get(i).getName());
+        }
+
+        // Creating adapter for listview
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_ListView_item, lables);
+
+
+
+        // attaching data adapter to spinner
+        listView.setAdapter(listViewAdapter);
+    }
+    /**
+     * Async task to get all food categories
+     * */
+    private class GetEstados extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Carregando os Estados..");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            ServiceHandler jsonParser = new ServiceHandler();
+            String json = jsonParser.makeServiceCall(URL_ESTADOS, ServiceHandler.GET);
+
+            Log.e("Response: ", "> " + json);
+
+            if (json != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(json);
+                    if (jsonObj != null) {
+                        JSONArray estados = jsonObj
+                                .getJSONArray("uf");
+
+                        for (int i = 0; i < estados.length(); i++) {
+                            JSONObject catObj = (JSONObject) estados.get(i);
+                            Estados uf = new Estados(catObj.getInt("id_uf"),
+                                    catObj.getString("uf"));
+                            estadosList.add(uf);
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                Log.e("JSON Data", "Didn't receive any data from server!");
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            populateTextView();
+        }
+
+    }
+
+
+    }
 
 
